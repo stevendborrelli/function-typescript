020c2d52cb67532c7583abdf33bcc3334eb5e13c
Steven Borrelli <steve@borrelli.org>
2024-10-18 15:38:38 -0500

    remove test file
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- test.js
+++
@@ -1,6 +0,0 @@
-const message = "hello world";
-console.log(message);
-function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-}
-greet("hello", new Date());
